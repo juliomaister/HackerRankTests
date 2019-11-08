80cf0ae1cf31f67866f87bb9caf164ca26cf55df
Julio Maisyrovicz <julio.maistrovicz@hotmail.com>
2019-11-08 00:23:17 -0200

    Projeto teste maven

---
+++ src/main/java/br/com/hacker/rank/App.java
@@ -0,0 +1,13 @@
+package br.com.hacker.rank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
